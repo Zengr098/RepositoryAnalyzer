f8a9c292f6443f7919c48f798b70dbba1e9f6790
Kevin Pacifico <kevinpacifico2001@gmail.com>
2022-08-20 22:13:01 +0200

    Parser metrics added on project

--- RepositoryAnalyzer/js/informations.js
+++ RepositoryAnalyzer/js/informations.js
@@ -4,20 +4,20 @@ window.onload = function(){
 
 //Funzione che permette di ottenere dall'URL l'ownerName e la repositoryName
 function extractNameRepo(){
-    var url = window.location.hash;
+    let url = window.location.hash;
     const myArray = url.split("/");
     fetchRepository(myArray[3], myArray[4]);
 }
 
 //Funzione che prende in input il proprietario della repository e la repository stessa producendo come risposta un JSON
 function fetchRepository(ownerName, repositoryName){
-    var requestURL = "https://api.github.com/repos/" + ownerName + "/" + repositoryName;
-    var request = $.get(requestURL, function () {}).done(function () {
+    let requestURL = "https://api.github.com/repos/" + ownerName + "/" + repositoryName;
+    let request = $.get(requestURL, function () {}).done(function () {
         request = request.responseJSON;
 
-        var contributors_url = request['contributors_url'];
-        var commits_url = request['commits_url'];
-        var issues_url = requestURL + "/issues/events";
+        let contributors_url = request['contributors_url'];
+        let commits_url = request['commits_url'];
+        let issues_url = requestURL + "/issues/events";
         commits_url = commits_url.slice(0, -6);
 
         showRepoInfo(request);
@@ -27,15 +27,15 @@ function fetchRepository(ownerName, repositoryName){
 
 // Funzione che mostra le informazioni sulla repository
 function showRepoInfo(request) {
-    var createYear = request.created_at.slice(0, -16);
-    var createMonth = request.created_at.slice(5, 7);
-    var createDay = request.created_at.slice(8, -10);
-    var createDate = createDay+"/"+createMonth+"/"+createYear;
+    let createYear = request.created_at.slice(0, -16);
+    let createMonth = request.created_at.slice(5, 7);
+    let createDay = request.created_at.slice(8, -10);
+    let createDate = createDay+"/"+createMonth+"/"+createYear;
 
-    var updateYear = request.updated_at.slice(0, -16);
-    var updateMonth = request.updated_at.slice(5, 7);
-    var updateDay = request.updated_at.slice(8, -10);
-    var updateDate = updateDay+"/"+updateMonth+"/"+updateYear;
+    let updateYear = request.updated_at.slice(0, -16);
+    let updateMonth = request.updated_at.slice(5, 7);
+    let updateDay = request.updated_at.slice(8, -10);
+    let updateDate = updateDay+"/"+updateMonth+"/"+updateYear;
 
     div = $(".repoName");
     nameRepo = request.name;
@@ -62,17 +62,17 @@ function showRepoInfo(request) {
 
 //Funzione che mette i contributors e i commit in un array
 function getContributors(contributors_url, commits_url, issues_url){
-    var request = $.get(contributors_url, function () {}).done(function () {
+    let request = $.get(contributors_url, function () {}).done(function () {
         request = request.responseJSON;
-        var contributors = [];
-        var data = [];
+        let contributors = [];
+        let data = [];
     
-        for(var i = 0; i < request.length; i++){
-            var name = request[i].login;
-            var image = request[i].avatar_url;
-            var ncommit = request[i].contributions;
+        for(let i = 0; i < request.length; i++){
+            let name = request[i].login;
+            let image = request[i].avatar_url;
+            let ncommit = request[i].contributions;
             
-            var contributor = {
+            let contributor = {
                 name: name,
                 ncommit: ncommit,
                 total: 0,
@@ -83,7 +83,11 @@ function getContributors(contributors_url, commits_url, issues_url){
                 openissue: 0,
                 closedissue: 0,
                 contributepercentage: 0,
-                bugpercentage: 0
+                bugpercentage: 0,
+                whiteline: 0,
+                comments: 0,
+                codelines: 0,
+                lines: 0            
             };
 
             data.push(image);
@@ -95,15 +99,15 @@ function getContributors(contributors_url, commits_url, issues_url){
 
 //Funzione che accede alle informazioni di ogni singola issue
 function fetchIssues(contributors, commits_url, issues_url, data){
-    var request = $.get(issues_url, function () {}).done(function () {
+    let request = $.get(issues_url, function () {}).done(function () {
         request = request.responseJSON;
-        var issues = [];
+        let issues = [];
     
-        for(var i = 0; i < request.length; i++){
-            var open = request[i].issue.user.login;
-            var close = request[i].actor.login;
+        for(let i = 0; i < request.length; i++){
+            let open = request[i].issue.user.login;
+            let close = request[i].actor.login;
             
-            var issue = {
+            let issue = {
                 open: open,
                 close: close
             };
@@ -116,12 +120,12 @@ function fetchIssues(contributors, commits_url, issues_url, data){
 
 //Funzione che accede alle informazioni di ogni singolo commit
 function getCommits(contributors, commits_url, issues, data){
-    var request = $.get(commits_url, function () {}).done(function () {
+    let request = $.get(commits_url, function () {}).done(function () {
         request = request.responseJSON;
-        var url = [];
+        let url = [];
             
-        for(var i=0; i < request.length; i++){
-            var commit = request[i].url;
+        for(let i=0; i < request.length; i++){
+            let commit = request[i].url;
             url.push(commit);
         }
         fetchCommits(contributors, url, issues, data);
@@ -130,7 +134,7 @@ function getCommits(contributors, commits_url, issues, data){
 
 //Funzioone che effettua la richiesta per prendere le informazioni di un singolo commit
 function requestCommit(sha){
-    var request = $.get(sha, function () {}).done(function () {
+    let request = $.get(sha, function () {}).done(function () {
         request = request.responseJSON;
     });
     return request;
@@ -138,45 +142,90 @@ function requestCommit(sha){
 
 //Funzione che salva i dati di tutti i commit in un array
 async function fetchCommits(contributors, url, issues, data){
-    var commits = [];
-    for(var i=0; i<url.length; i++){
-        var sha = url[i];
+    let commits = [];
+    for(let i=0; i<url.length; i++){
+        let sha = url[i];
         const request = await requestCommit(sha);
         
-        var name = request.committer.login;
-        var total = request.stats.total;
-        var additions = request.stats.additions;
-        var deletions = request.stats.deletions;
-        var nfile = request.files.length;
-        var lines = [];
+        let name = request.committer.login;
+        let total = request.stats.total;
+        let additions = request.stats.additions;
+        let deletions = request.stats.deletions;
+        let nfile = request.files.length;
+        let whiteline = 0;
+        let comments = 0;
+        let code = 0;
 
-        for(var j=0; j<request.files.length; j++){
+        for(let j=0; j<request.files.length; j++){
+            let lines = [];
             if(request.files[j].patch){
-                var file = request.files[j].patch.replaceAll(' ', '');
+                let file = request.files[j].patch.replaceAll(' ', '');
                 lines = lines.concat(file.split("\n"));
-                for(var k=0; k<lines.length; k++){
-                    //controllo se la riga inizia con +
+                for(let k=0; k<lines.length; k++){
+                    //controllo se la riga è una riga aggiunta
                     if(lines[k].startsWith("+")){
-                        //cancello il + e la lascio nell'array
                         lines[k] = lines[k].substring(1);
+                        if(lines[k].startsWith("//")){
+                            comments++;
+                            lines.splice(k, 1);
+                            k--;
+                        }
+                        else if(lines[k] == ""){
+                            whiteline++;
+                            lines.splice(k, 1);
+                            k--;
+                        }
+                        else if(lines[k].startsWith("/*")){
+                            for(let l=k; l<lines.length; l++){
+                                if(lines[l].includes("*/")){
+                                   comments++;
+                                   lines.splice(l, 1);
+                                   l--;
+                                   break;
+                                }
+                                else{
+                                    comments++;
+                                    lines.splice(l, 1);
+                                    l--;
+                                }
+                            }
+                        }
+                        else if(lines[k].startsWith("<!--")){
+                            for(let m=k; m<lines.length; m++){
+                                if(lines[m].includes("--!")){
+                                   comments++;
+                                   lines.splice(m, 1);
+                                   m--;
+                                   break; 
+                                }
+                                else{
+                                    comments++;
+                                    lines.splice(m, 1);
+                                    m--;
+                                }
+                            }
+                        }
+                        else{
+                            code++;
+                            lines.splice(k, 1);
+                            k--;
+                        }
                     }
-                    //se non inizia con + la cancello perchè non è una riga aggiunta
-                    else{
-                        lines.splice(k, 1);
-                        k--;
-                    }
-                    //PER OGNI FILE SALVARE LE METRICHE IN 3 VARIABILI E SVUOTARE L'ARRAY 
                 }
-            } 
+            }
         }
 
-        var commit = {
+        let nlines = whiteline+comments+code;
+        let commit = {
             name: name,
             total: total,
             additions: additions,
             deletions: deletions,
             nfile: nfile,
-            lines: lines
+            whiteline: whiteline,
+            comments: comments,
+            code: code,
+            lines: nlines
         }
 
         commits.push(commit);
@@ -186,29 +235,41 @@ async function fetchCommits(contributors, url, issues, data){
 
 //Funzione che prende le linee di codice di ogni contributors
 function updateInformation(contributors, commits, issues, data){
-    for(var i=0; i<commits.length; i++){
-        for(var j=0; j<contributors.length; j++){
-            var total = contributors[j].total;
-            var additions = contributors[j].additions;
-            var deletions = contributors[j].deletions;
-            var nfile = contributors[j].nfile;
+    for(let i=0; i<commits.length; i++){
+        for(let j=0; j<contributors.length; j++){
+            let total = contributors[j].total;
+            let additions = contributors[j].additions;
+            let deletions = contributors[j].deletions;
+            let nfile = contributors[j].nfile;
+            let whiteline = contributors[j].whiteline;
+            let comments = contributors[j].comments;
+            let codelines = contributors[j].codelines;
+            let lines = contributors[j].lines;
 
-            var newtotal = commits[i].total;
-            var newadditions = commits[i].additions;
-            var newdeletions = commits[i].deletions;
-            var newfile = commits[i].nfile;
+            let newtotal = commits[i].total;
+            let newadditions = commits[i].additions;
+            let newdeletions = commits[i].deletions;
+            let newfile = commits[i].nfile;
+            let newwhite = commits[i].whiteline;
+            let newcomments = commits[i].comments;
+            let newcode = commits[i].code;
+            let newlines = commits[i].lines;
 
             if(commits[i].name == contributors[j].name){
                 contributors[j].total = total + newtotal;
                 contributors[j].additions = additions + newadditions;
                 contributors[j].deletions = deletions + newdeletions;
                 contributors[j].nfile = nfile + newfile;
+                contributors[j].whiteline = whiteline + newwhite;
+                contributors[j].comments = comments + newcomments;
+                contributors[j].codelines = codelines + newcode;
+                contributors[j].lines = lines + newlines;
             }
         }
     }
 
-    for(var t=0; t<issues.length; t++){
-        for(var l=0; l<contributors.length; l++){
+    for(let t=0; t<issues.length; t++){
+        for(let l=0; l<contributors.length; l++){
             if(issues[t].open == contributors[l].name){
                 contributors[l].openissue ++;
             }
@@ -218,9 +279,9 @@ function updateInformation(contributors, commits, issues, data){
         }
     }
 
-    var total = 0;
-    var bug = 0;
-    for (var k=0; k<contributors.length; k++){
+    let total = 0;
+    let bug = 0;
+    for (let k=0; k<contributors.length; k++){
         contributors[k].lineforcommit = (contributors[k].additions/contributors[k].ncommit).toFixed(2);
         contributors[k].nfile = (contributors[k].nfile/contributors[k].ncommit).toFixed(2);
 
@@ -228,26 +289,29 @@ function updateInformation(contributors, commits, issues, data){
         bug += contributors[k].closedissue;
     }
 
-    for (var h=0; h<contributors.length; h++){
+    for (let h=0; h<contributors.length; h++){
         contributors[h].contributepercentage = ((contributors[h].additions * 100) / total).toFixed(2);
         contributors[h].bugpercentage = ((contributors[h].closedissue * 100) / bug).toFixed(2);
+        contributors[h].whiteline = ((contributors[h].whiteline * 100) / contributors[h].lines).toFixed(2);
+        contributors[h].comments = ((contributors[h].comments * 100) / contributors[h].lines).toFixed(2);
+        contributors[h].codelines = ((contributors[h].codelines * 100) / contributors[h].lines).toFixed(2);
     }
     showInformations(contributors, data);
 }
 
 // Funzione che mostra a video tutte le informazioni
 function showInformations(contributors, data) {
-    var container = document.getElementById("containerParent");
+    let container = document.getElementById("containerParent");
 
-    for(var i=0; i<contributors.length; i++){
-        var divClone = container.cloneNode(true);
-        var id = "containerParent-" + i;
+    for(let i=0; i<contributors.length; i++){
+        let divClone = container.cloneNode(true);
+        let id = "containerParent-" + i;
         divClone.id = id;
 
-        var divImage = divClone.getElementsByClassName("containerImages")[0];
+        let divImage = divClone.getElementsByClassName("containerImages")[0];
         showImageContributors(divImage, data[i]);
         
-        var divContributor = divClone.getElementsByClassName("containerContributors")[0];
+        let divContributor = divClone.getElementsByClassName("containerContributors")[0];
 
         pname = divContributor.querySelector(".username");
         pcommit = divContributor.querySelector(".commit");
@@ -260,6 +324,9 @@ function showInformations(contributors, data) {
         pclose = divContributor.querySelector(".close");
         pcontribute = divContributor.querySelector(".contribute");
         pbug = divContributor.querySelector(".bug");
+        pcode = divContributor.querySelector(".codelines");
+        pcomments = divContributor.querySelector(".comments");
+        pwhite = divContributor.querySelector(".whitelines");
 
         pname.textContent = "Developer username: "+ contributors[i].name;
         pcommit.textContent = "Number of commit: "+contributors[i].ncommit;
@@ -271,13 +338,35 @@ function showInformations(contributors, data) {
         popen.textContent = "Number of opened issues: "+contributors[i].openissue;
         pclose.textContent = "Number of closed issue: "+contributors[i].closedissue;
         pcontribute.textContent = "Contribute percentage: "+contributors[i].contributepercentage+"%";
-        var bug = contributors[i].bugpercentage;
+        let bug = contributors[i].bugpercentage;
+        let codelines = contributors[i].codelines;
+        let comments = contributors[i].comments;
+        let whitelines = contributors[i].whiteline;
         if(isNaN(bug)){
             pbug.textContent = "Issues fixed percentage: 0.00%";
         }
         else{
-            pbug.textContent = "Issues fixed percentage: "+contributors[i].bugpercentage+"%";
+            pbug.textContent = "Issues fixed percentage: "+bug+"%";
         }
+        if(isNaN(codelines)){
+            pcode.textContent = "Code lines percentage: 0.00%";
+        }
+        else{
+            pcode.textContent = "Code lines percentage: "+codelines+"%";
+        }
+        if(isNaN(comments)){
+            pcomments.textContent = "Documentation percentage: 0.00%";
+        }
+        else{
+            pcomments.textContent = "Documentation percentage: "+comments+"%";
+        }
+        if(isNaN(whitelines)){
+            pwhite.textContent = "White lines percentage: 0.00%";
+        }
+        else{
+            pwhite.textContent = "White lines percentage: "+whitelines+"%";
+        }
+
 
         document.getElementById("fill").append(divClone);
     }
@@ -286,6 +375,6 @@ function showInformations(contributors, data) {
 
 // Funzione che mostra l'immagine dello sviluppatore
 function showImageContributors(divImage, data) {
-    var containerHtml="<br><img class=\"img-fluid\" src=\""+data+" alt=\"...\" \/>";
+    let containerHtml="<br><img class=\"img-fluid\" src=\""+data+" alt=\"...\" \/>";
     divImage.innerHTML = divImage.innerHTML + containerHtml;
 }
